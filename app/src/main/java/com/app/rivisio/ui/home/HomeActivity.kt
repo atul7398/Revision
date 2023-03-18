2da047b4be4171b750aa300fdcd2839539fe8d25
Vipin Kumar <vipin.kumar@moengage.com>
2023-03-18 19:06:15 +0530

    add login screen

--- app/src/main/java/com/app/rivisio/ui/home/HomeActivity.kt
+++ app/src/main/java/com/app/rivisio/ui/home/HomeActivity.kt
@@ -1,10 +1,17 @@
 package com.app.rivisio.ui.home
 
+import android.content.Context
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.app.rivisio.R
 
 class HomeActivity : AppCompatActivity() {
+
+    companion object {
+        fun getStartIntent(context: Context) = Intent(context, HomeActivity::class.java)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
--- app/src/main/java/com/app/rivisio/ui/login/LoginActivity.kt
+++ app/src/main/java/com/app/rivisio/ui/login/LoginActivity.kt
@@ -4,7 +4,9 @@ import android.content.Context
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.widget.AppCompatButton
 import com.app.rivisio.R
+import com.app.rivisio.ui.home.HomeActivity
 
 class LoginActivity : AppCompatActivity() {
 
@@ -15,5 +17,10 @@ class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        findViewById<AppCompatButton>(R.id.google_button).setOnClickListener {
+            startActivity(HomeActivity.getStartIntent(this@LoginActivity))
+            finish()
+        }
     }
 }
