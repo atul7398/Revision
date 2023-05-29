8c6b7b3fae5c5d6b4f797b87333ffe7092de736e
Vipin <vicky7230@gmail.com>
2023-05-29 07:46:55 +0530

    add code in referral activity

--- app/src/main/java/com/app/rivisio/ui/refer/ReferActivity.kt
+++ app/src/main/java/com/app/rivisio/ui/refer/ReferActivity.kt
@@ -1,10 +1,17 @@
 package com.app.rivisio.ui.refer
 
+import android.content.ClipData
+import android.content.ClipboardManager
 import android.content.Context
 import android.content.Intent
 import android.os.Bundle
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.AppCompatImageView
+import com.app.rivisio.BuildConfig
 import com.app.rivisio.R
+import es.dmoral.toasty.Toasty
+
 
 class ReferActivity : AppCompatActivity() {
 
@@ -15,5 +22,41 @@ class ReferActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_refer)
+
+        findViewById<AppCompatImageView>(R.id.back_button).setOnClickListener { finish() }
+
+        findViewById<AppCompatImageView>(R.id.copy_referral_code).setOnClickListener {
+            val clipboard = getSystemService(CLIPBOARD_SERVICE) as ClipboardManager
+            val clip = ClipData.newPlainText("Referral code", "XXXXXXXXXX")
+            clipboard.setPrimaryClip(clip)
+
+            Toasty.custom(
+                this,
+                "Referral code coped to clipboard",
+                R.drawable.ic_info,
+                es.dmoral.toasty.R.color.infoColor,
+                Toast.LENGTH_SHORT,
+                true,
+                true
+            ).show()
+        }
+
+        findViewById<AppCompatImageView>(R.id.refer1).setOnClickListener { shareApp() }
+        findViewById<AppCompatImageView>(R.id.refer2).setOnClickListener { shareApp() }
+        findViewById<AppCompatImageView>(R.id.refer3).setOnClickListener { shareApp() }
+        findViewById<AppCompatImageView>(R.id.refer4).setOnClickListener { shareApp() }
+        findViewById<AppCompatImageView>(R.id.refer5).setOnClickListener { shareApp() }
+
+    }
+
+    private fun shareApp() {
+        val sendIntent = Intent()
+        sendIntent.action = Intent.ACTION_SEND
+        sendIntent.putExtra(
+            Intent.EXTRA_TEXT,
+            "Hey check out my app at: https://play.google.com/store/apps/details?id=" + BuildConfig.APPLICATION_ID
+        )
+        sendIntent.type = "text/plain"
+        startActivity(sendIntent)
     }
 }
